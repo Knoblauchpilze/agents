fab1d09e262c919c77f69087ea69d97dcfa790ae
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2022-04-04 16:03:00 +0200

    fix: Fixed an issue where objects would be considered infinitely massive for no reason, and also added friction

--- src/game/physics/MovingObject.cc
+++ src/game/physics/MovingObject.cc
@@ -1,6 +1,11 @@
 
 # include "MovingObject.hh"
 
+/// @brief - The decrease in speed due to friction and other
+/// processing at each frame. This value is expressed in m/s2.
+/// This correspond to an acceleration.
+# define FRICTION_ACCELERATION 0.9f
+
 namespace mas {
   namespace environment {
 
@@ -55,12 +60,15 @@ namespace mas {
       float d = manager.lastStepDuration(time::Unit::Second);
 
       // Apply force to modify acceleration if needed.
-      if (m_body.infinitelyMassive()) {
+      if (!m_body.infinitelyMassive()) {
         m_acceleration = (1.0f / m_body.mass()) * m_force;
       }
 
       // Apply acceleration to modify speed.
       m_speed += (m_acceleration * d);
+
+      // Account for friction.
+      m_speed *= (1.0f - FRICTION_ACCELERATION * d);
 
       // Apply speed to modify position.
       utils::Vector2f tr = m_speed * d;
@@ -68,7 +76,18 @@ namespace mas {
       m_area.x() += tr.x();
       m_area.y() += tr.y();
 
-      log("Moving object at " + m_area.toString(), utils::Level::Verbose);
+      log(
+        "Moving object at " + m_area.toString() +
+        " (f: " + m_force.toString() +
+        ", acc: " + m_acceleration.toString() +
+        ", speed: " + m_speed.toString() +
+        ", friction: " + std::to_string((1.0f - FRICTION_ACCELERATION) * d) + ")",
+        utils::Level::Verbose
+      );
+
+      // Reset the force: if it is applied continuously we
+      // leave the other processes to reapply it.
+      m_force = utils::Vector2f();
     }
 
     void
